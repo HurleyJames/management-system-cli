82e9d65054c78cecff4ce3ab855425132f5e0eba
Hurley <1401682479@qq.com>
2021-05-20 21:46:36 +0800

    initial: init project

---
+++ src/main/java/ac/hurley/managementsystemcli/ManagementSystemCliApplication.java
@@ -0,0 +1,13 @@
+package ac.hurley.managementsystemcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSystemCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementSystemCliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/hurley/managementsystemcli/ManagementSystemCliApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.hurley.managementsystemcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
