0871a8bd5bc272bcad92a936dd92987a44d5b801
Hurley <1401682479@qq.com>
2021-05-23 17:05:15 +0800

    feat: 添加 logpack-spring.xml 和 generator.properties

--- src/main/java/ac/hurley/managementsystemcli/ManagementSystemCliApplication.java
+++ src/main/java/ac/hurley/managementsystemcli/ManagementSystemCliApplication.java
@@ -1,13 +1,34 @@
 package ac.hurley.managementsystemcli;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+import java.net.InetAddress;
+
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@MapperScan("ac.hurley.managementsystemcli.mapper")
+@Slf4j
 public class ManagementSystemCliApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ManagementSystemCliApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ManagementSystemCliApplication.class, args);
+
+        Environment environment = applicationContext.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Login: \thttp://{}:{}/login\n\t" +
+                        "Doc: \thttp://{}:{}/doc.html\n" +
+                        "----------------------------------------------------------",
+                environment.getProperty("spring.application.name"),
+                InetAddress.getLocalHost().getHostAddress(),
+                environment.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                environment.getProperty("server.port"));
     }
 
 }
